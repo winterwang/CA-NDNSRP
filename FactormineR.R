fabe98e062ffa69d297b51a2293347dafa3a620d
ChaochenWang <chaochen@wangcc.me>
2019-07-12 17:25:51 +0900

    updated CA using factorminer and factoextra

--- FactormineR.R
+++ FactormineR.R
@@ -44,9 +44,82 @@ barplot(res.ca$eig[,1],main="Eigenvalues",
 
 res.ca2 <- CA(work,col.sup=4:ncol(work))
 
+##%######################################################%##
+#                                                          #
+####                olympic medals data                 ####
+#                                                          #
+##%######################################################%##
 
 data(JO)
 res.jo <- CA(JO)
 summary(res.jo)
 
 plot(res.jo, cex = 0.6)
+
+
+##%######################################################%##
+#                                                          #
+####                     Wine data                      ####
+#                                                          #
+##%######################################################%##
+
+data.wine = read.table("http://factominer.free.fr/bookV2/wine.csv",
+                       header=TRUE,row.names=1,sep=";",check.names=FALSE)
+res.ca=CA(data.wine,col.sup=11,row.sup=nrow(data.wine))
+
+barplot(res.ca$eig[,1],main="Eigenvalues", 
+        names.arg=1:nrow(res.ca$eig))
+
+summary(res.ca, nb.dec=2, ncp=2)
+
+plot(res.ca)
+
+
+##%######################################################%##
+#                                                          #
+####                   mortality data                   ####
+#                                                          #
+##%######################################################%##
+
+
+death <- read.table("http://factominer.free.fr/bookV2/death.csv",
+                    header=TRUE,sep=";",row.names=1)
+colnames(death) <- c("0-1","1-4","5-14","15-24","25-34","35-44",
+                     "45-54","55-64","65-74","75-84","85-94","95+")
+
+
+res.ca <- CA(death,row.sup=66:nrow(death), graph=FALSE)
+plot(res.ca)
+round(res.ca$call$marge.col,3)
+round(res.ca$call$marge.row[order(res.ca$call$marge.row)],3)
+par(las=1)
+barplot(res.ca$call$marge.col,horiz=TRUE)
+barplot(res.ca$call$marge.row[order(res.ca$call$marge.row)],horiz=TRUE)
+par(las=0)
+res.ca <- CA(death,row.sup=66:nrow(death))
+summary(res.ca, nb.dec=4)
+
+# decompose the inertia by row and by column
+100*res.ca$col$inertia/sum(res.ca$col$inertia)
+
+100*res.ca$row$inertia[rev(order(res.ca$row$inertia))]/sum(res.ca$row$inertia)
+
+
+bb<-round(cbind.data.frame(res.ca$call$marge.col,
+                           sqrt(res.ca$col$inertia/res.ca$call$marge.col),
+                           res.ca$col$inertia,res.ca$col$inertia/sum(res.ca$col$inertia)),4)
+colnames(bb)<-c("Weight","Distance","Inertia","% of inertia")
+bb
+
+
+plot.CA(res.ca, axes = c(1, 2), autoLab = "yes", cex = 0.7)
+
+library(factoextra)
+fviz_ca_biplot(res.ca, label ="col", 
+               invisible = c("row", "row.sup"))
+
+fviz_ca_biplot(res.ca, label = c("col","row"), 
+               # invisible = c("row.sup"),
+               select.row = list(contrib = 20),
+               repel = T)
+
