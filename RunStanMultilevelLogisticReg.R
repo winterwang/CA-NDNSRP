37e920222fbc14d11e268ef069aa957a8c062075
wangcc.me <abelardccwang@gmail.com>
2019-08-18 22:13:35 +0900

    stan file done

--- RunStanMultilevelLogisticReg.R
+++ RunStanMultilevelLogisticReg.R
@@ -1,6 +1,11 @@
 library(brms)
 library(tidyverse)
 library(naniar)
+TFood <- TFood %>% 
+  left_join(Indiv1_9_adlt[,c("seriali", "nssec8", "area")], by = "seriali")
+names(TFood)[85] <- "DM4cat"
+TFood <- TFood[, -82]
+TFood <- TFood[, -c(6, 10, 11)]
 
 TFood <- TFood %>% 
   mutate(YesPudding = as.numeric(MainFoodGroupCode == 9)) %>% 
@@ -12,12 +17,65 @@ TFood <- TFood %>%
          nssec8 = as.factor(nssec8)) 
 
 dataForStan <- TFood %>% 
-  select(seriali, DM4cat, Agecat, nssec8, Time3g, Sex, area) %>% 
+  select(seriali, YesPudding, DM4cat, Agecat, nssec8, Time3g, Sex, area) %>% 
   mutate(Sex = as.factor(Sex))
 
-dataForStan <- fastDummies::dummy_cols(dataForStan, remove_first_dummy = TRUE)
+dataForStan <- fastDummies::dummy_cols(dataForStan, select_columns = c("nssec8",
+                                                                       "Agecat",
+                                                                       "DM4cat",
+                                                                       "Time3g"),
+                                       remove_first_dummy = TRUE, ignore_na = TRUE)
 
+dataForStan <- dataForStan %>% 
+  mutate(Agecat_1 = `Agecat_[41,51)`,
+         Agecat_2 = `Agecat_[51,61)`,
+         Agecat_3 = `Agecat_[61,71)`, 
+         Agecat_4 = `Agecat_[71,81)`,
+         Agecat_5 = `Agecat_[81,100)`)
+
+Indiv1_9_adlt <- Indiv1_9_adlt %>% 
+  mutate(Agecat = cut(age, breaks = c(18, 41, 51, 61, 71, 81, 100), right = FALSE)) %>% 
+  replace_with_na(replace = list(nssec8 = -9))
+
+  
+Indiv1_9_adlt <- fastDummies::dummy_cols(Indiv1_9_adlt, select_columns = c("DM4cat", 
+                                                                           "nssec8", 
+                                                                           "Agecat"),
+                                         remove_first_dummy = TRUE, ignore_na = TRUE)
+Indiv1_9_adlt <- Indiv1_9_adlt %>% 
+  mutate(Agecat_1 = `Agecat_[41,51)`,
+         Agecat_2 = `Agecat_[51,61)`,
+         Agecat_3 = `Agecat_[61,71)`, 
+         Agecat_4 = `Agecat_[71,81)`,
+         Agecat_5 = `Agecat_[81,100)`)
+
+length(unique(dataForStan$seriali))
+
+Indiv1_9_adlt$ID <- seq.int(nrow(Indiv1_9_adlt))
+dataForStan <- dataForStan %>% 
+  left_join(Indiv1_9_adlt[, c("seriali", "ID")],  by = "seriali")
+
+# use brm package
 fit <- brm(YesPudding ~ Time3g + DM4cat + Sex + Agecat + nssec8 + (1 | seriali), data = TFood,
            family = bernoulli("logit"))
 summary(fit)
 
+# write my own stan program
+library(rstan)
+
+data <- list(N = 6802, I = 374344, DM4cat_1 = Indiv1_9_adlt$DM4cat_1, DM4cat_2 = Indiv1_9_adlt$DM4cat_2,
+             DM4cat_3 = Indiv1_9_adlt$DM4cat_3, Agecat_1 = Indiv1_9_adlt$Agecat_1, 
+             Agecat_2 = Indiv1_9_adlt$Agecat_2, Agecat_3 = Indiv1_9_adlt$Agecat_3, 
+             Agecat_4 = Indiv1_9_adlt$Agecat_4, Agecat_5 = Indiv1_9_adlt$Agecat_5,
+             nssec8_1 = Indiv1_9_adlt$nssec8_1, nssec8_2 = Indiv1_9_adlt$nssec8_2, 
+             nssec8_3 = Indiv1_9_adlt$nssec8_3, nssec8_4 = Indiv1_9_adlt$nssec8_4, 
+             nssec8_5 = Indiv1_9_adlt$nssec8_5, nssec8_6 = Indiv1_9_adlt$nssec8_6,
+             nssec8_8 = Indiv1_9_adlt$nssec8_8, seriali = dataForStan$ID, 
+             Time3g_Afternoon = dataForStan$Time3g_Afternoon, 
+             Time3g_Evening = dataForStan$Time3g_Evening, 
+             YesPudding = dataForStan$YesPudding)
+
+
+library(rstan)
+fit0 <- stan(file = "runStanProgram/NDNSRP.stan", data = data, 
+             pars = c("b", "b_P", "s_P", "q", "OR_Eve_vs_Morn", "OR_Eve_vs_After"), seed = 1234)
--- runStanProgram/NDNSRP.stan
+++ runStanProgram/NDNSRP.stan
@@ -1,20 +1,65 @@
 data {
   int N;
   int I;  
-  int<lower = 0, upper = 1> 
+  int<lower = 0, upper = 1> DM4cat_1[N]; 
+  int<lower = 0, upper = 1> DM4cat_2[N]; 
+  int<lower = 0, upper = 1> DM4cat_3[N];
+  int<lower = 0, upper = 1> Agecat_1[N];
+  int<lower = 0, upper = 1> Agecat_2[N];
+  int<lower = 0, upper = 1> Agecat_3[N];
+  int<lower = 0, upper = 1> Agecat_4[N];
+  int<lower = 0, upper = 1> Agecat_5[N];
+  int<lower = 0, upper = 1> nssec8_1[N];
+  int<lower = 0, upper = 1> nssec8_2[N];
+  int<lower = 0, upper = 1> nssec8_3[N];
+  int<lower = 0, upper = 1> nssec8_4[N];
+  int<lower = 0, upper = 1> nssec8_5[N];
+  int<lower = 0, upper = 1> nssec8_6[N];
+  int<lower = 0, upper = 1> nssec8_8[N];
+  int<lower = 1, upper = N> seriali[I];
+  int<lower = 0, upper = 1> Time3g_Afternoon[I];
+  int<lower = 0, upper = 1> Time3g_Evening[I];
+  int<lower = 0, upper = 1> YesPudding[I];
 }
 
 // The parameters accepted by the model. Our model
 // accepts two parameters 'mu' and 'sigma'.
 parameters {
-  real mu;
-  real<lower=0> sigma;
+  real b[18];
+  real b_P[N];
+  real<lower = 0> s_P;
 }
 
-// The model to be estimated. We model the output
-// 'y' to be normally distributed with mean 'mu'
-// and standard deviation 'sigma'.
-model {
-  y ~ normal(mu, sigma);
+transformed parameters {
+  real x_P[N];
+  real x_J[I];
+  real x[I];
+  real q[I];
+  for (n in 1:N)
+    x_P[n] = b[2]*DM4cat_1[n] + b[3]*DM4cat_2[n] + b[4]*DM4cat_3[n] + 
+             b[5]*Agecat_1[n] + b[6]*Agecat_2[n] + b[7]*Agecat_3[n] + 
+             b[8]*Agecat_4[n] + b[9]*Agecat_5[n] + b[10]*nssec8_1[n] + 
+             b[11]*nssec8_2[n] + b[12]*nssec8_3[n] + b[13]*nssec8_4[n] + 
+             b[14]*nssec8_5[n] + b[15]*nssec8_6[n] + b[16]*nssec8_8[n] + 
+             b_P[n];
+  for (i in 1:I) {
+    x_J[i] = b[17]*Time3g_Afternoon[i] + b[18]*Time3g_Evening[i];
+    x[i]   = b[1] + x_P[seriali[i]] + x_J[i];
+    q[i]   = inv_logit(x[i]);
+  }
 }
 
+model{
+  for (n in 1:N)
+    b_P[n] ~ normal(0, s_P);
+  for (i in 1:I)
+    YesPudding[i]  ~ bernoulli(q[i]);
+}
+
+
+generated quantities{
+  real OR_Eve_vs_Morn; 
+  real OR_Eve_vs_After;
+  OR_Eve_vs_Morn = exp(b[18]);
+  OR_Eve_vs_After = exp(b[18] - b[17]);
+}
